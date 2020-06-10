3ba9f5e10e82c15ee33e2477aca584882e641099
Jakub Havelka <jakub.havelka@atos.net>
2020-06-10 13:11:58 +0200

    Let Angular handle routes instead of Spring Boot. Closes #3

--- media-store/media-store-web/src/main/java/com/jakub/media/store/controllers/BaseController.java
+++ media-store/media-store-web/src/main/java/com/jakub/media/store/controllers/BaseController.java
@@ -1,15 +1,14 @@
 package com.jakub.media.store.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BaseController {
 
-    @RequestMapping({"", "/"})
-    public String welcome(){
+  //  @RequestMapping({"", "/"})
+  //  public String welcome(){
 
-        return "Welcome to Media Store";
-    }
+  //      return "Welcome to Media Store";
+  //  }
 
 }
