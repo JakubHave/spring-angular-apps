6b13144bf519ad36f57a8f37865e029ee5a035e1
Jakub Havelka <jakub.havelka@atos.net>
2020-06-10 13:17:59 +0200

    Fix - ComponentScan not needed

--- media-store/media-store-web/src/main/java/com/jakub/media/store/MediaStoreApplication.java
+++ media-store/media-store-web/src/main/java/com/jakub/media/store/MediaStoreApplication.java
@@ -2,9 +2,7 @@ package com.jakub.media.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages={"com.jakub.media.store"})
 @SpringBootApplication
 public class MediaStoreApplication {
 
